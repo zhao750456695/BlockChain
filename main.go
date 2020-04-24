c82c2387284960ef85237acabcf64609f1f3ac88
zhao750456695 <zhao750456695@163.com>
2020-04-24 11:53:19 +0800

    添加创世块

--- main.go
+++ main.go
@@ -38,19 +38,37 @@ func (block *Block) SetHash() {
 }
 
 //4.引入区块链
-//
+type BlockChain struct {
+	// 定义一个区块链数组
+	blocks []*Block
+}
+
+// 5.定义一个区块链
+func NewBlockChain() *BlockChain{
+	// 创建一个创世块，并作为第一个区块添加到区块链中
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+
+// 创世块
+func GenesisBlock() *Block{
+	return NewBlock("创世块", []byte{})
+}
+
 //5.添加区块
 //
 //6.重构代码
 
 func main() {
 
-	block := NewBlock("a to b 1 coin", []byte{})
-
-	fmt.Printf("前区块哈希： %x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希： %x\n", block.Hash)
-	fmt.Printf("区块数据： %s\n", block.Data)
-
-	fmt.Printf("hello btc")
+	bc := NewBlockChain()
+	for i, block := range bc.blocks{
+		fmt.Printf("当前区块高度 %d \r\n", i)
+		fmt.Printf("前区块哈希： %x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希： %x\n", block.Hash)
+		fmt.Printf("区块数据： %s\n", block.Data)
+	}
 }
 
