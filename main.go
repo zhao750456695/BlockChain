4fab0015f19cfc24514601f15107e77de1140189
zhao750456695 <zhao750456695@163.com>
2020-04-24 12:08:27 +0800

    添加区块

--- main.go
+++ main.go
@@ -57,15 +57,30 @@ func GenesisBlock() *Block{
 	return NewBlock("创世块", []byte{})
 }
 
-//5.添加区块
-//
+//6.添加区块
+func (bc *BlockChain)AddBlock(data string)  {
+
+	// 获取最后一个区块
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	prevHash := lastBlock.Hash
+
+	// 创建新的区块
+	block := NewBlock(data, prevHash)
+
+	// 添加到区块链数组中
+	bc.blocks = append(bc.blocks, block)
+}
+
+
 //6.重构代码
 
 func main() {
 
 	bc := NewBlockChain()
+	bc.AddBlock("第二个块")
+	bc.AddBlock("第三个块")
 	for i, block := range bc.blocks{
-		fmt.Printf("当前区块高度 %d \r\n", i)
+		fmt.Printf("========= 当前区块高度 %d =========\r\n", i)
 		fmt.Printf("前区块哈希： %x\n", block.PrevHash)
 		fmt.Printf("当前区块哈希： %x\n", block.Hash)
 		fmt.Printf("区块数据： %s\n", block.Data)
